955e6f701e6293a738161b2cd3796a5cc8d32512
Jon Jampen <jon.jampen@cryptography.ch>
2023-03-14 14:27:12 +0100

    Creating user with firebase auth and email password.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,27 @@
 <script>
-    import "./signInAnonymously";
-</script>
-<h1>The Cyber Crusade</h1>
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { app } from "./initializeFirebase";
 
-<p>Logged in as: </p>
+let email, password;
+const auth = getAuth(app);
+
+function signUpUser() {	
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage)
+    });
+}
+</script>
+
+<form on:submit={signUpUser} id="signUpForm">
+    <input type="text" name="email" id="signUpEmailInput" bind:value={email}>
+    <input type="password" name="password" id="signUpPasswordInput" bind:value={password}>
+    <button type="submit" id="signupSubmit">Sign Up</button>
+</form>
