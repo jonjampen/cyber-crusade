8d64daeb034a77662902579bfb4d856cc769d8bf
Jon Jampen <jon.jampen@cryptography.ch>
2023-03-16 13:09:20 +0100

    Adding logout function

---
+++ src/routes/logout/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+import { getAuth, signOut } from "firebase/auth";
+import { app } from "../initializeFirebase";
+
+const auth = getAuth(app);
+
+signOut(auth).then(() => {
+  // Sign-out successful.
+  console.log("logged out")
+}).catch((error) => {
+    console.log("not logged out")
+  // An error happened.
+});    
+</script>
