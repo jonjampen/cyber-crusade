578bd2b9fcfc659978f182ab9a2e7991f722b53e
Jon Jampen <jon.jampen@cryptography.ch>
2023-03-19 10:30:19 +0100

    Adding player to collection after button click.

--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -1,12 +1,39 @@
 <script>
-    function getCookieValue(name) {
-        return document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || '' //https://stackoverflow.com/a/25490531/17760960
-    }
+    import authStore from "../../stores/authStore";
+    import { app } from "../initializeFirebase";
+    import { getAuth } from "firebase/auth";
+    import { getFirestore, addDoc, collection } from "firebase/firestore";
+    
+    let playState = null;
+
     function joinGame() {
-        console.log(getCookieValue("userData").email)
+
+        authStore.subscribe(async ({ user }) => {
+            if (user) {
+                // check if already in players collection
+                const auth = getAuth(app);
+                const db = getFirestore(app);
+                const playersColl = collection(db, "players")
+
+                let data = {
+                    uid: user.uid,
+                }
+                addDoc(playersColl, data);
+                playState = "joined";
+
+                console.log(user.email)
+            }
+        });
     }
+
+    //subscribe to players collection
 
 </script>
 
 <h1>Play!</h1>
-<button on:click={joinGame}>Join Game</button>
+
+{#if !playState}
+    <button on:click={joinGame}>Join Game</button>
+{:else if playState == "joined"}
+    <p>You joined the game</p>
+{/if}
