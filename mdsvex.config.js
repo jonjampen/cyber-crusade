5c315889f135cf217ccb7ae4c4e9f04c3c3e8093
Jon Jampen <jon.jampen@cryptography.ch>
2023-04-25 14:18:18 +0200

    trying to resolve bug.

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -4,12 +4,11 @@ import { defineMDSveXConfig as defineConfig } from 'mdsvex';
 const config = defineConfig({
 	extensions: ['.svelte.md', '.md', '.svx'],
 
-	smartypants: {
-		dashes: 'oldschool'
-	},
-
-	remarkPlugins: [],
-	rehypePlugins: []
+    preprocess: [
+        mdsvex({
+            extensions: ['.md', '.svx'],
+        })
+    ],
 });
 
 export default config;
