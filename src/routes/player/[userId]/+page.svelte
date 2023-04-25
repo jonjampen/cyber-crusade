34ed829b984a10aedbbd40188921ca41f597dfe9
Jon Jampen <jon.jampen@cryptography.ch>
2023-04-25 18:44:34 +0200

    Adding apexcharts.

--- src/routes/player/[userId]/+page.svelte
+++ src/routes/player/[userId]/+page.svelte
@@ -1,4 +1,19 @@
 <script>
+  import { chart } from "svelte-apexcharts";
+  let options = {
+    chart: {
+      type: "bar",
+    },
+    series: [
+      {
+        name: "sales",
+        data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+      },
+    ],
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+    },
+  };
 
 </script>
 
@@ -20,3 +35,5 @@
         lose chart
     </div>
 </div>
+
+    <div class="chart" use:chart={options} stlye="color: black !important"/>
