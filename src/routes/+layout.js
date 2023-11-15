c395fa0d2ebe4f12036ed4089034f19780b4ef98
Jon Jampen <jon.jampen@cryptography.ch>
2023-11-15 15:40:02 +0100

    Random name if no name.

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -3,11 +3,11 @@ import { firebaseAuth, firebaseDb } from "../lib/firebase";
 import { authUser } from '$lib/stores/authStore';
 import { browser } from "$app/environment";
 import { query, where, collection, getDocs } from "firebase/firestore";
+import { randomName } from "$lib/randomName";
 
 export async function load({ url }) {
     if (browser) {
         // keep updating auth
-
         onAuthStateChanged(firebaseAuth, async (user) => {
             if (user) {
                 let users = await getDocs(query(collection(firebaseDb, "users"), where("uid", "==", user.uid)));
@@ -15,6 +15,8 @@ export async function load({ url }) {
                 users.forEach(a => {
                     name = a.data().name
                 })
+
+                name = name || randomName()
 
                 authUser.set({
                     uid: user.uid,
