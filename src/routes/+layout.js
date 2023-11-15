7498be47b390d2fd532e27eb584c96812862a919
Jon Jampen <jon.jampen@cryptography.ch>
2023-11-15 15:36:32 +0100

    Actual name.

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,16 +1,24 @@
 import { onAuthStateChanged } from "firebase/auth";
-import { firebaseAuth } from "../lib/firebase";
+import { firebaseAuth, firebaseDb } from "../lib/firebase";
 import { authUser } from '$lib/stores/authStore';
 import { browser } from "$app/environment";
+import { query, where, collection, getDocs } from "firebase/firestore";
 
 export async function load({ url }) {
     if (browser) {
         // keep updating auth
-        onAuthStateChanged(firebaseAuth, (user) => {
+
+        onAuthStateChanged(firebaseAuth, async (user) => {
             if (user) {
+                let users = await getDocs(query(collection(firebaseDb, "users"), where("uid", "==", user.uid)));
+                let name = "";
+                users.forEach(a => {
+                    name = a.data().name
+                })
+
                 authUser.set({
                     uid: user.uid,
-                    name: "Peter",
+                    name: name,
                     email: user.email,
                 })
             }
